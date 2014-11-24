1e987bc84b00ca27e15ff3f45d4c317fc3f9d18e
HE XIAO <hexiao2@illinois.edu>
2014-11-24 16:18:44 -0600

    unfinished

--- RVM/analysis/LogMonitor.java
+++ RVM/analysis/LogMonitor.java
@@ -17,27 +17,14 @@ import java.util.List;
  * Created by xiaohe on 11/24/14.
  */
 public class LogMonitor {
-    /**
-     * These are the event names.
-     * Can gen them via analyzing all the events in sig file.
-     */
-    public static final String PUBLISH = "publish";
-    public static final String APPROVE = "approve";
 
-    public static HashMap<String, Integer[]> TableCol = new HashMap<>();
+    private HashMap<String, Integer[]> TableCol;
 
-    private static HashMap<String, Class[]> MethodArgListMap = new HashMap<>();
+    private HashMap<String, Class[]> MethodArgListMap = new HashMap<>();
 
-    private static Class MonitorClass;
+    private Class monitorClass;
 
-    private static void init() {
-        //the arg types can be inferred from the signature file
-        Integer[] argTy4Publish = new Integer[]{RegHelper.INT_TYPE};
-        Integer[] argTy4Approve = new Integer[]{RegHelper.INT_TYPE};
-        TableCol.put(PUBLISH, argTy4Publish);
-        TableCol.put(APPROVE, argTy4Approve);
-
-        MonitorClass=PubRuntimeMonitor.class;
+    private void init() {
 
         for (String tableName: TableCol.keySet()){
             Integer[] types4CurTable= TableCol.get(tableName);
--- src/fsl/uiuc/Main.java
+++ src/fsl/uiuc/Main.java
@@ -1,6 +1,34 @@
 package fsl.uiuc;
 
+import reg.RegHelper;
+import rvm.PubRuntimeMonitor;
+
+import java.util.HashMap;
+
 public class Main {
+
+    /**
+     * These are the event names.
+     * Can gen them via analyzing all the events in sig file.
+     */
+    public static final String PUBLISH = "publish";
+    public static final String APPROVE = "approve";
+
+    public static HashMap<String, Integer[]> TableCol = new HashMap<>();
+
+    private static HashMap<String, Class[]> MethodArgListMap = new HashMap<>();
+
+    private static Class MonitorClass;
+
+    private static void init() {
+        //the arg types can be inferred from the signature file
+        Integer[] argTy4Publish = new Integer[]{RegHelper.INT_TYPE};
+        Integer[] argTy4Approve = new Integer[]{RegHelper.INT_TYPE};
+        TableCol.put(PUBLISH, argTy4Publish);
+        TableCol.put(APPROVE, argTy4Approve);
+
+        MonitorClass = PubRuntimeMonitor.class;
+    }
 
     public static void main(String[] args) {
         // write your code here
