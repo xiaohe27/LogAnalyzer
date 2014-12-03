da9712dfc9f00f2f038454abfb1a129bdcb72d83
HE XIAO <hexiao2@illinois.edu>
2014-12-03 16:43:19 -0600

    update a little

--- RVM/gen/MonitorGenerator.java
+++ RVM/gen/MonitorGenerator.java
@@ -11,48 +11,79 @@ import java.util.List;
 /**
  * Created by xiaohe on 12/3/14.
  * Generate a monitor lib according to the template below and instantiation parameters.
+ * <p/>
+ * package rvm;
+ * <p/>
+ * Pub(Integer report) {
+ * Integer report;
+ * long time;
+ * <p/>
+ * event publish (Integer report, long time) {
+ * this.report=report;
+ * this.time=time;
+ * }
+ * <p/>
+ * event approve (Integer report, long time) {
+ * this.report=report;
+ * this.time=time;
+ * }
+ * <p/>
+ * ltl: [](publish => (*) approve)
  *
- package rvm;
-
- Pub(Integer report) {
- Integer report;
- long time;
-
- event publish (Integer report, long time) {
- this.report=report;
- this.time=time;
- }
-
- event approve (Integer report, long time) {
- this.report=report;
- this.time=time;
- }
-
- ltl: [](publish => (*) approve)
-
- @violation { System.out.println("should not publish financial report "+this.report+" without pre-approval");}
-
- }
-
+ * @violation { System.out.println("should not publish financial report "+this.report+" without pre-approval");}
+ * <p/>
+ * }
  */
 public class MonitorGenerator {
 
     private File sigFile;
     private FormulaExtractor formulaExtractor;
 
-    public MonitorGenerator(Path sigFilePath, Path formulaFilePath){
-        this.sigFile= sigFilePath.toFile();
-        this.formulaExtractor=new FormulaExtractor(formulaFilePath);
+    private StringBuilder sb = new StringBuilder();
+
+    public MonitorGenerator(Path sigFilePath, Path formulaFilePath) {
+        this.sigFile = sigFilePath.toFile();
+        this.formulaExtractor = new FormulaExtractor(formulaFilePath);
 
         this.genMonitorLib();
     }
 
     private void genMonitorLib() {
         //to be implemented.
+        genImports();
+
+        genPackage();
+
+        genMonitorMain();
+
+        genPhysicalFile();
+    }
+
+    private void genPhysicalFile() {
+        //fake path
+        String monitorLibPath = "./test/rvm/PubTest.rvm";
+
+        Utils.writeToFile(this.sb.toString(), monitorLibPath);
+    }
+
+    private void genMonitorMain() {
+
+    }
+
+    private void genPackage() {
+        sb.append("\npackage rvm;\n");
+    }
+
+    private void genImports() {
+        sb.append("\nimport java.io.IOException;\n" +
+                "import java.nio.file.Files;\n" +
+                "import java.nio.file.Path;\n" +
+                "import java.nio.file.Paths;\n" +
+                "import java.nio.file.StandardOpenOption;\n");
     }
 
 
-    public String getMonitorClassPath(){
+    public String getMonitorClassPath() {
         return this.formulaExtractor.getMonitorName();
     }
 
@@ -60,7 +91,7 @@ public class MonitorGenerator {
         return this.formulaExtractor.getMethodNameList();
     }
 
-    public HashMap<String, Integer[]> getMethoArgsMappingFromSigFile(){
+    public HashMap<String, Integer[]> getMethoArgsMappingFromSigFile() {
         return SigExtractor.extractMethoArgsMappingFromSigFile(sigFile);
     }
 
