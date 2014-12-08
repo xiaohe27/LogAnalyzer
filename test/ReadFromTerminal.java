3c414675c1015a2356d5e814c97c445d051f8802
HE XIAO <hexiao2@illinois.edu>
2014-12-08 12:01:19 -0600

    try to read input from terminal via piping

---
+++ test/ReadFromTerminal.java
@@ -0,0 +1,20 @@
+class ReadFromTerminal{
+public static void main(String[] args){
+InputStreamReader isReader = new InputStreamReader(System.in);
+BufferedReader bufReader = new BufferedReader(isReader);
+while(true){
+    try {
+        String inputStr = null;
+        if((inputStr=bufReader.readLine()) != null) {
+            System.out.println("Input is "+inputStr);
+        }
+        else {
+           break;
+        }
+    }
+    catch (Exception e) {
+       
+    }
+}
+}
+}
--- test/TestLogMonitor.java
+++ test/TestLogMonitor.java
@@ -17,14 +17,18 @@ public class TestLogMonitor {
 
     }
 
-    @Test
-    public void testMain() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
+//    @Test
+    public void testMain1() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
         String[] args=new String[]{"./test/insert-hugeLog/insert.sig","./test/insert-hugeLog/insert.fl",
                                      "./test/insert-hugeLog/insert.log"};
         Main.main(args);
+    }
 
-        //after refactoring, assert whether the log file has been fully read
-        //by testing whether hasNext() returns false;
 
+    @Test
+    public void testMain() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
+        String[] args=new String[]{"./test/insert-hugeLog/insert.sig","./test/insert-hugeLog/insert.fl",
+                "/home/xiaohe/SW/offline-log-analysis/ldcc.csv"};
+        Main.main(args);
     }
 }
