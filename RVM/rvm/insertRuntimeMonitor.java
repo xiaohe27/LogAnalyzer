fd0dcb9299468d2ae2c4cda845353e7ee8db2b07
HE XIAO <hexiao2@illinois.edu>
2014-12-16 14:23:58 -0600

    save cur work

--- RVM/rvm/insertRuntimeMonitor.java
+++ RVM/rvm/insertRuntimeMonitor.java
@@ -1,12 +1,20 @@
 package rvm;
-import com.runtimeverification.rvmonitor.java.rt.RuntimeOption;
-import com.runtimeverification.rvmonitor.java.rt.ref.CachedWeakReference;
-import com.runtimeverification.rvmonitor.java.rt.table.MapOfMap;
-import com.runtimeverification.rvmonitor.java.rt.table.MapOfMonitor;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
+import java.util.*;
+import java.lang.ref.*;
+import com.runtimeverification.rvmonitor.java.rt.*;
+import com.runtimeverification.rvmonitor.java.rt.ref.*;
+import com.runtimeverification.rvmonitor.java.rt.table.*;
+import com.runtimeverification.rvmonitor.java.rt.tablebase.AbstractIndexingTree;
+import com.runtimeverification.rvmonitor.java.rt.tablebase.SetEventDelegator;
+import com.runtimeverification.rvmonitor.java.rt.tablebase.TableAdopter.Tuple2;
+import com.runtimeverification.rvmonitor.java.rt.tablebase.TableAdopter.Tuple3;
+import com.runtimeverification.rvmonitor.java.rt.tablebase.IDisableHolder;
+import com.runtimeverification.rvmonitor.java.rt.tablebase.IMonitor;
+import com.runtimeverification.rvmonitor.java.rt.tablebase.DisableHolder;
 import com.runtimeverification.rvmonitor.java.rt.tablebase.TerminatedMonitorCleaner;
-
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicInteger;
 
 final class InsertRawMonitor_Set extends com.runtimeverification.rvmonitor.java.rt.tablebase.AbstractMonitorSet<InsertRawMonitor> {
 
@@ -43,12 +51,6 @@ class InsertRawMonitor extends com.runtimeverification.rvmonitor.java.rt.tableba
 		}
 	}
 
-	String user;
-	String db;
-	String p;
-	String data;
-	long time;
-
 	@Override
 	public final int getState() {
 		return -1;
@@ -76,22 +78,9 @@ class InsertRawMonitor extends com.runtimeverification.rvmonitor.java.rt.tableba
 		RVM_lastevent = -1;
 	}
 
-	// RVMRef_user was suppressed to reduce memory overhead
-	// RVMRef_db was suppressed to reduce memory overhead
-	// RVMRef_p was suppressed to reduce memory overhead
-	// RVMRef_data was suppressed to reduce memory overhead
-
 	@Override
 	protected final void terminateInternal(int idnum) {
 		switch(idnum){
-			case 0:
-			break;
-			case 1:
-			break;
-			case 2:
-			break;
-			case 3:
-			break;
 		}
 		switch(RVM_lastevent) {
 			case -1:
@@ -119,17 +108,11 @@ public final class insertRuntimeMonitor implements com.runtimeverification.rvmon
 	private static boolean Insert_activated = false;
 
 	// Declarations for Indexing Trees
-	private static Object Insert_user_db_p_data_Map_cachekey_data;
-	private static Object Insert_user_db_p_data_Map_cachekey_db;
-	private static Object Insert_user_db_p_data_Map_cachekey_p;
-	private static Object Insert_user_db_p_data_Map_cachekey_user;
-	private static InsertRawMonitor Insert_user_db_p_data_Map_cachevalue;
-	private static final MapOfMap<MapOfMap<MapOfMap<MapOfMonitor<InsertRawMonitor>>>> Insert_user_db_p_data_Map = new MapOfMap<MapOfMap<MapOfMap<MapOfMonitor<InsertRawMonitor>>>>(0) ;
+	private static final InsertRawMonitor Insert__Map = new InsertRawMonitor() ;
 
 	public static int cleanUp() {
 		int collected = 0;
 		// indexing trees
-		collected += Insert_user_db_p_data_Map.cleanUpUnnecessaryMappings();
 		return collected;
 	}
 
@@ -148,73 +131,19 @@ public final class insertRuntimeMonitor implements com.runtimeverification.rvmon
 			Thread.yield();
 		}
 
-		CachedWeakReference wr_p = null;
-		CachedWeakReference wr_data = null;
-		CachedWeakReference wr_user = null;
-		CachedWeakReference wr_db = null;
-		MapOfMonitor<InsertRawMonitor> matchedLastMap = null;
 		InsertRawMonitor matchedEntry = null;
-		boolean cachehit = false;
-		if (((((data == Insert_user_db_p_data_Map_cachekey_data) && (db == Insert_user_db_p_data_Map_cachekey_db) ) && (p == Insert_user_db_p_data_Map_cachekey_p) ) && (user == Insert_user_db_p_data_Map_cachekey_user) ) ) {
-			matchedEntry = Insert_user_db_p_data_Map_cachevalue;
-			cachehit = true;
-		}
-		else {
-			wr_user = new CachedWeakReference(user) ;
-			wr_db = new CachedWeakReference(db) ;
-			wr_p = new CachedWeakReference(p) ;
-			wr_data = new CachedWeakReference(data) ;
-			{
-				// FindOrCreateEntry
-				MapOfMap<MapOfMap<MapOfMonitor<InsertRawMonitor>>> node_user = Insert_user_db_p_data_Map.getNodeEquivalent(wr_user) ;
-				if ((node_user == null) ) {
-					node_user = new MapOfMap<MapOfMap<MapOfMonitor<InsertRawMonitor>>>(1) ;
-					Insert_user_db_p_data_Map.putNode(wr_user, node_user) ;
-				}
-				MapOfMap<MapOfMonitor<InsertRawMonitor>> node_user_db = node_user.getNodeEquivalent(wr_db) ;
-				if ((node_user_db == null) ) {
-					node_user_db = new MapOfMap<MapOfMonitor<InsertRawMonitor>>(2) ;
-					node_user.putNode(wr_db, node_user_db) ;
-				}
-				MapOfMonitor<InsertRawMonitor> node_user_db_p = node_user_db.getNodeEquivalent(wr_p) ;
-				if ((node_user_db_p == null) ) {
-					node_user_db_p = new MapOfMonitor<InsertRawMonitor>(3) ;
-					node_user_db.putNode(wr_p, node_user_db_p) ;
-				}
-				matchedLastMap = node_user_db_p;
-				InsertRawMonitor node_user_db_p_data = node_user_db_p.getNodeEquivalent(wr_data) ;
-				matchedEntry = node_user_db_p_data;
-			}
+		{
+			// FindOrCreateEntry
+			matchedEntry = Insert__Map;
 		}
 		// D(X) main:1
 		if ((matchedEntry == null) ) {
-			if ((wr_user == null) ) {
-				wr_user = new CachedWeakReference(user) ;
-			}
-			if ((wr_db == null) ) {
-				wr_db = new CachedWeakReference(db) ;
-			}
-			if ((wr_p == null) ) {
-				wr_p = new CachedWeakReference(p) ;
-			}
-			if ((wr_data == null) ) {
-				wr_data = new CachedWeakReference(data) ;
-			}
 			// D(X) main:4
 			InsertRawMonitor created = new InsertRawMonitor() ;
 			matchedEntry = created;
-			matchedLastMap.putNode(wr_data, created) ;
 		}
 		// D(X) main:8--9
 		matchedEntry.event_insert(user, db, p, data, time);
-
-		if ((cachehit == false) ) {
-			Insert_user_db_p_data_Map_cachekey_data = data;
-			Insert_user_db_p_data_Map_cachekey_db = db;
-			Insert_user_db_p_data_Map_cachekey_p = p;
-			Insert_user_db_p_data_Map_cachekey_user = user;
-			Insert_user_db_p_data_Map_cachevalue = matchedEntry;
-		}
 
 		insert_RVMLock.unlock();
 	}
