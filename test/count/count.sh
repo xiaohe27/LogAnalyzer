91401e45b2f805469f4231a9fa70791deedc2564
HE XIAO <hexiao2@illinois.edu>
2015-01-10 17:07:06 -0600

    update some test results

--- test/count/count.sh
+++ test/count/count.sh
@@ -1,9 +1,13 @@
-Start=$(date +"%s")
-echo "Start time : $Start" > time.txt
+PC=Siebel
 
-java -cp /home/xiaohe/Projects/LogAnalyzer/production:$CLASSPATH fsl.uiuc.Main insert.sig insert.fl /home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly 
+Out="count-$PC-MyLogAnalyzer.txt"
+
+Start=$(date +"%s")
+echo "Start time : $Start" > $Out
+
+java -cp /home/xiaohe/Projects/LogAnalyzer/production:$CLASSPATH fsl.uiuc.Main insert.sig insert.fl /home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly >> $Out
 
 End=$(date +"%s")
-echo "Finish time : $End" >> time.txt
+echo "\nFinish time : $End" >> $Out
 Diff=$(( $End - $Start ))
-echo "\nIt took $Diff seconds to count all the events in the 9M log ldcc4Monpoly" >> time.txt
+echo "\nIt took my log analyzer $Diff seconds to count all the events in the 9M log ldcc4Monpoly at $PC" >> $Out
