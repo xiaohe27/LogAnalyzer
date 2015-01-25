a3d89ea48114688524ad62aa37fcce302c0675e8
HE XIAO <hexiao2@illinois.edu>
2015-01-25 15:09:59 -0600

    clean test suites

--- test/insert-hugeLog/bigLog.sh
+++
@@ -1,9 +0,0 @@
-Start=$(date +"%s")
-echo "Start time : $Start" > time.txt
-
-./csv2log 4 /home/xiaohe/SW/offline-log-analysis/ldcc.csv | sh logAnalyzer-2args.sh > insert2HugeLog.txt
-
-End=$(date +"%s")
-echo "Finish time : $End" >> time.txt
-Diff=$(( $End - $Start ))
-echo "\nIt took $Diff seconds to analyze the insert property of the huge log ldcc.csv" >> time.txt
