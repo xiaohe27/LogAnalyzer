15a76258e8425717b0a3a3ba570d4fa12441bd24
HE XIAO <hexiao2@illinois.edu>
2015-01-27 14:32:56 -0600

    impl the testcase gen

--- src/util/TestCaseGen.java
+++ src/util/TestCaseGen.java
@@ -1,7 +1,27 @@
 package util;
 
+import sig.SigExtractor;
+
 /**
  * Created by xiaohe on 1/27/15.
  */
 public class TestCaseGen {
+    public static void genTestCase(String nameOfGeneratedDataFile, int numOfLogEntries) {
+        StringBuilder sb= new StringBuilder(10000);
+
+        for (int i = 0; i < numOfLogEntries; i++) {
+            sb.append("@" + i);
+            sb.append(" "+ SigExtractor.APPROVE+"("+i+") ");
+            sb.append( SigExtractor.PUBLISH+"("+i+")\n");
+        }
+
+        Utils.writeToFile(sb.toString(), "/home/xiaohe/workspace/DATA/FakeData4TestingPerf/"+nameOfGeneratedDataFile);
+    }
+
+    public static void main(String[] args) {
+        int sizeOfDataFile = 2000000;
+
+        genTestCase("Pub_fake.log", sizeOfDataFile);
+
+    }
 }
